33c621058f7abe417d99bf25bcd3f1a2f626c9b5
IwaiAtsuki <atk@M-Atsuki-Iwai-MBP.local>
2017-10-22 21:16:19 +0900

    implement async processing

--- app/src/main/java/com/example/atk/weatherforecasts/MainActivity.java
+++ app/src/main/java/com/example/atk/weatherforecasts/MainActivity.java
@@ -1,47 +1,44 @@
 package com.example.atk.weatherforecasts;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
 import android.os.Handler;
+import android.content.Context;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private TextView textView;
-    private Handler handler;
+
+    private class GetWeatherForecastTask extends GetWeatherForecastApiTask {
+        public GetWeatherForecastTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+
+            if (data != null) {
+                textView.setText(data);
+            } else if(exception != null) {
+                Toast.makeText(MainActivity.this,
+                        exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.tv_main);
 
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    final String data = WeatherApi.getWeather(MainActivity.this,
-                            "40040");
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            textView.setText(data);
-                        }
-                    });
-                } catch (IOException e) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, e.getMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            }
-        };
-        thread.start();
+        new GetWeatherForecastTask(this).execute("40040");
     }
 }
